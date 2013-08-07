848fae4d48f3cef63572d8902f5542d96070095d
Chris Baughman <cmartinbaughman@gmail.com>
2013-08-07 18:28:27 +0000

    Attempting to fix dislpay for 4.3

--- common.mk
+++ common.mk
@@ -4,6 +4,7 @@ common_includes += hardware/qcom/display-legacy/libgenlock
 common_includes += hardware/qcom/display-legacy/liboverlay
 common_includes += hardware/qcom/display-legacy/libcopybit
 common_includes += hardware/qcom/display-legacy/libqdutils
+#common_includes += hardware/qcom/display-legacy/libhwcomposer
 
 ifeq ($(TARGET_USES_POST_PROCESSING),true)
     common_flags     += -DUSES_POST_PROCESSING
@@ -36,9 +37,7 @@ endif
 common_deps :=
 kernel_includes :=
 
-ifneq ($(TARGET_PREBUILT_HEADERS),true)
 ifeq ($(call is-vendor-board-platform,QCOM),true)
      common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
      kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
  endif
-endif
--- liboverlay/Android.mk
+++ liboverlay/Android.mk
@@ -7,6 +7,7 @@ LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libqdutils
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"overlay\"
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES := \
       overlay.cpp \
       overlayCtrl.cpp \
